5cad08c7474287f4c5d8fb64dd49a901ea208840
Vijay_1289 <vijay.siruvuru@gmail.com>
2026-03-05 21:23:29 +0530

    feat: Configure Next.js to allow remote images from `lh3.googleusercontent.com`.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Authentication routes need server-side functions
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
